e3cd78e083f27def7c8752d386e4248b10341823
titan <titan@titans-Mac-Studio.local>
2024-01-21 15:45:31 -0800

    chore

--- python/cmd1.py
+++ python/cmd1.py
@@ -2,11 +2,12 @@ import subprocess
 
 command = "ls -l"  # Replace with your desired Bash command
 
-result = subprocess.run(command, shell=True, capture_output=True, text=True)
+result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 if result.returncode == 0:
     output = result.stdout
-    print("Command output:", output)
+    for line in result.stdout.splitlines():
+        print(line.decode('utf-8'))
 else:
     error = result.stderr
     print("Command error:", error)
