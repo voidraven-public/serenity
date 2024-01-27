293264b1e55bb7195cc9ab8466bb7ed60d8a5e1c
titan <titan@titans-Mac-Studio.local>
2024-01-26 21:09:29 -0800

    chore

--- boto3/code_deploy_details.py
+++ boto3/code_deploy_details.py
@@ -3,9 +3,15 @@ import boto3
 # Initialize a Boto3 CodeDeploy client
 codedeploy_client = boto3.client('codedeploy')
 
-# List all CodeDeploy applications
-response = codedeploy_client.list_applications()
+# List all deployment groups
+response = codedeploy_client.list_application_revisions(
+    applicationName='Your-Application-Name'  # Replace with your application name
+)
 
-# Iterate through the applications and print their names
-for application_name in response['applications']:
-    print(f"CodeDeploy Application Name: {application_name}")
+# Iterate through the deployment jobs and print their names and service roles
+for deployment_job in response['revisions']:
+    deployment_job_name = deployment_job['revisionLabel']
+    service_role_arn = deployment_job['genericRevisionInfo']['serviceRoleArn']
+
+    print(f"Deployment Job Name: {deployment_job_name}")
+    print(f"Service Role ARN: {service_role_arn}\n")
